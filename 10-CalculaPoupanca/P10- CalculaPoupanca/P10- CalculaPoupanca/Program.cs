5944e5dc8490b1c0639264fc8e8b1aae543f8ce4
DESKTOP-KDJSS8E\lolra <lolrainy@gmail.com>
2020-12-09 23:57:42 -0300

    Incluindo novos projetos e estudos

---
+++ 10-CalculaPoupanca/P10- CalculaPoupanca/P10- CalculaPoupanca/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                if (contadorMes == 1)
+                {
+                    Console.WriteLine("Após " + contadorMes + " mês, você terá " + valorInvestido);
+                    contadorMes += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Após " + contadorMes + " meses, você terá " + valorInvestido);
+                    contadorMes += 1;
+                }
+                
+                //contadorMes = contadorMes + 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P11-CalculaPoupança2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca2
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes = contadorMes + 1)
+            {
+                valorInvestido *= 1.0036;
+                if (contadorMes == 1)
+                {
+                    Console.WriteLine(
+                       "após " + contadorMes +
+                       " mês, você terá R$ " + valorInvestido);
+                }
+                else {             
+                    Console.WriteLine(
+                        "após " + contadorMes +
+                        " meses, você terá R$ " + valorInvestido);
+                    }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
